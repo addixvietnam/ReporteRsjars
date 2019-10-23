b0237b3dd5be356fbbb71155f3924609bc17513b
nhanlamaddix <nhan.lam@addix.vn>
2019-10-23 17:26:15 +0700

    This is old library. So I get from github to backup library

---
+++ R/tools.R
@@ -0,0 +1,11 @@
+.onLoad= function(libname, pkgname){
+  .jpackage(name = pkgname, jars = "*")
+
+  jv <- .jcall("java/lang/System", "S", "getProperty", "java.runtime.version")
+  if(substr(jv, 1L, 2L) == "1.") {
+    jvn <- as.numeric(paste0(strsplit(jv, "[.]")[[1L]][1:2], collapse = "."))
+    if(jvn < 1.6) stop("Java >= 6 is needed for this package but not available")
+  }
+
+  invisible()
+}
